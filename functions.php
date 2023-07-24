e582c9be9c0622c8d7612f53a00a7ae2dcc80e1c
Luis Cabrera Benito <parzibyte@gmail.com>
2023-07-24 10:33:40 -0600

    Nav

--- functions.php
+++ functions.php
@@ -1,5 +1,14 @@
 <?php
 //Remove Gutenberg Block Library CSS from loading on the frontend
+function register_my_menus() {
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+      'footer' => __( 'Footer menu' ),
+     )
+   );
+ }
+ add_action( 'init', 'register_my_menus' );
 function smartwp_remove_wp_block_library_css()
 {
     wp_dequeue_style('wp-block-library');
@@ -43,3 +52,42 @@ remove_action('wp_print_styles', 'print_emoji_styles');
 
 remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('admin_print_styles', 'print_emoji_styles');
+function disable_embeds_code_init() {
+
+ // Remove the REST API endpoint.
+ remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+
+ // Turn off oEmbed auto discovery.
+ add_filter( 'embed_oembed_discover', '__return_false' );
+
+ // Don't filter oEmbed results.
+ remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+
+ // Remove oEmbed discovery links.
+ remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+
+ // Remove oEmbed-specific JavaScript from the front-end and back-end.
+ remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+ add_filter( 'tiny_mce_plugins', 'disable_embeds_tiny_mce_plugin' );
+
+ // Remove all embeds rewrite rules.
+ add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+
+ // Remove filter of the oEmbed result before any HTTP requests are made.
+ remove_filter( 'pre_oembed_result', 'wp_filter_pre_oembed_result', 10 );
+}
+
+add_action( 'init', 'disable_embeds_code_init', 9999 );
+
+function disable_embeds_tiny_mce_plugin($plugins) {
+    return array_diff($plugins, array('wpembed'));
+}
+
+function disable_embeds_rewrites($rules) {
+    foreach($rules as $rule => $rewrite) {
+        if(false !== strpos($rewrite, 'embed=true')) {
+            unset($rules[$rule]);
+        }
+    }
+    return $rules;
+}
--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,20 +8,12 @@
     <?php wp_head(); ?>
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 </head>
+
 <body <?php body_class(); ?>>
     <header class="site-header">
-        <div class="container">
-            <h1 class="site-title">
-                <a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-            </h1>
-            <nav class="site-navigation">
-                <?php
-                wp_nav_menu(array(
-                    'theme_location' => 'primary',
-                    'container' => false,
-                    'menu_class' => 'primary-menu',
-                ));
-                ?>
-            </nav>
-        </div>
+        <?php wp_nav_menu(array(
+            'theme_location' => 'header-menu',
+            'container' => 'nav',
+            "menu_class" => "border border-gray-500 justify-center font-medium flex flex-col p-4  mt-4  rounded-lg  md:flex-row md:space-x-8 md:mt-0 bg-white"
+        )); ?>
     </header>
