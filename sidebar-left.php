b57dbf34b47d3cc2faaa7943371729c66adddf85
Luis Cabrera Benito <parzibyte@gmail.com>
2023-07-31 19:06:06 -0600

    Update sidebar-left.php

--- sidebar-left.php
+++ sidebar-left.php
@@ -1,5 +1,3 @@
-<!-- Barra izquierda en dispositivos grandes -->
     <div id="sidebar-left" class="md:w-1/4 sm:w-full  order-1 sm:order-2 ">
-
-	<?php dynamic_sidebar('left'); ?>
+        <?php dynamic_sidebar('left'); ?>
     </div>
